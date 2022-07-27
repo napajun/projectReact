6c920d097b18072892b148f4eeb0fd466cc93a7a
napajun <napajun2545@gmail.com>
2022-07-27 09:28:13 +0700

    commit date 27-Jul/22

--- App.js
+++ App.js
@@ -5,10 +5,11 @@ import Logo from "./component/Logo";
 // import Count from "./components/Count";
 import InputText from "./components/InputText";
 import UserNameandPassWord from "./components/UserNameandPassWord";
+import TouchablePractice from "./components/TouchablePractice";
 const App = () => {
   return (
     <View>
-      <UserNameandPassWord />
+      <TouchablePractice />
     </View>
   );
 };
